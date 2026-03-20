8e4d080cb6fd4cee4a082ee10b9894365b1ce47e
Gabriel <166523386+Marcfr01@users.noreply.github.com>
2026-03-20 19:50:10 -0300

    Reorganised some the indentation

--- Exp04/controlador.vhd
+++ Exp04/controlador.vhd
@@ -30,29 +30,29 @@ begin
 	
 	-- Lógica de próximo estado
 	proximo_estado <= INICIAL when (estado_atual = INICIAL and liga  = '0') else 
-							INICIAL when (estado_atual = ESPERA  and liga  = '0') else 
-							LIGADO  when (estado_atual = INICIAL and liga  = '1') else
-							LIGADO  when (estado_atual = LIGADO  and sinal = '0') else 
-							PREPARA when (estado_atual = LIGADO  and sinal = '1') else 
-							PREPARA when (estado_atual = ESPERA  and (sinal = '1' and liga = '1')) else 
-							CONTA   when (estado_atual = PREPARA) else 
-							CONTA   when (estado_atual = CONTA   and sinal = '1') else 
-							FIM     when (estado_atual = CONTA   and sinal = '0') else 
-							ESPERA  when (estado_atual = FIM     and sinal = '1') else 
-							ESPERA  when (estado_atual = ESPERA  and (sinal = '0' and liga = '1')) else 
-							estado_atual;
+					  INICIAL when (estado_atual = ESPERA  and liga  = '0') else 
+					  LIGADO  when (estado_atual = INICIAL and liga  = '1') else
+					  LIGADO  when (estado_atual = LIGADO  and sinal = '0') else 
+					  PREPARA when (estado_atual = LIGADO  and sinal = '1') else 
+					  PREPARA when (estado_atual = ESPERA  and (sinal = '1' and liga = '1')) else 
+					  CONTA   when (estado_atual = PREPARA) else 
+					  CONTA   when (estado_atual = CONTA   and sinal = '1') else 
+					  FIM     when (estado_atual = CONTA   and sinal = '0') else 
+					  ESPERA  when (estado_atual = FIM     and sinal = '1') else 
+					  ESPERA  when (estado_atual = ESPERA  and (sinal = '0' and liga = '1')) else 
+					  estado_atual;
 						
 	-- Saídas 
 	with estado_atual select zeraCont   <= '1' when PREPARA, '0' when others;
-	with estado_atual select contaCont 	<= '1' when CONTA, 	'0' when others;
-	with estado_atual select pronto     <= '1' when FIM, 		'0' when others;
+	with estado_atual select contaCont 	<= '1' when CONTA, 	 '0' when others;
+	with estado_atual select pronto     <= '1' when FIM, 	 '0' when others;
 	
 	db_estado <= "0001" when (estado_atual = INICIAL) else
-                "0010" when (estado_atual = LIGADO)  else
-                "0011" when (estado_atual = PREPARA) else
-                "0100" when (estado_atual = CONTA)   else
-                "0101" when (estado_atual = FIM)     else
-                "0110" when (estado_atual = ESPERA)  else
-                "0000";
+                 "0010" when (estado_atual = LIGADO)  else
+                 "0011" when (estado_atual = PREPARA) else
+                 "0100" when (estado_atual = CONTA)   else
+                 "0101" when (estado_atual = FIM)     else
+                 "0110" when (estado_atual = ESPERA)  else
+                 "0000";
 	
 end architecture;
