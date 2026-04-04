3b31cfe877f921c4abf10f667e8c41235457a9b1
Diggs0 <rodrigoasatsuma@gmail.com>
2026-04-03 21:01:11 -0300

    Create interface_leds_botoes.vhd

---
+++ Exp05/interface_leds_botoes.vhd
@@ -0,0 +1,35 @@
+entity interface_leds_botoes is
+ port (
+ clock : in std_logic;
+ reset : in std_logic;
+ iniciar : in std_logic;
+ resposta : in std_logic;
+ ligado : out std_logic;
+ estimulo : out std_logic;
+ pulso : out std_logic;
+ erro : out std_logic;
+ pronto : out std_logic
+ );
+end entity interface_leds_botoes;
+
+architecture arch of interface_led_botoes is
+	--declaracoes dos componentes utilizados
+	component UC is
+	 port (
+		 clock : in std_logic;
+		 reset : in std_logic;
+		 iniciar : in std_logic;
+		 resposta : in std_logic;
+		 passou10 : in std_logic;
+		 ligado : out std_logic;
+		 estimulo : out std_logic;
+		 --pulso : out std_logic;
+		 erro : out std_logic;
+		 pronto : out std_logic;
+		 db_estado : out std_logic
+	 );
+	end component;
+
+	begin
+
+end architecture;
