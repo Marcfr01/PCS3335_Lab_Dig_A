02fe05c01b7e11a3e5e7230d6742ffe352587c11
Diggs0 <rodrigoasatsuma@gmail.com>
2026-05-16 12:00:08 -0300

    sweden

--- Exp09/ControleBuzzerNotas.vhd
+++ Exp09/ControleBuzzerNotas.vhd
@@ -7,7 +7,7 @@ entity ControleBuzzerNotas is
         clk       : in  STD_LOGIC;                         -- Clock de entrada (50 MHz)
         rst       : in  STD_LOGIC;                         -- Reset assíncrono
         en        : in  STD_LOGIC;                         -- '1' liga o som, '0' desliga
-        nota_in   : in  STD_LOGIC_VECTOR(3 downto 0);      -- Seleciona a nota (0 a 12)
+        nota_in   : in  STD_LOGIC_VECTOR(2 downto 0);      -- Seleciona a nota (0 a 12)
         buzz_out  : out STD_LOGIC                          -- Saída para o buzzer passivo
     );
 end ControleBuzzerNotas;
@@ -16,22 +16,17 @@ architecture Comportamento of ControleBuzzerNotas is
 
     type tabela_notas is array (0 to 12) of integer;
     
-    -- Referencia Frequencia da notas: https://iazzetta.eca.usp.br/tutor/acustica/introducao/tabela1.html
+    -- Referencia Frequencia da notas: https://mixbutton.com/music-tools/frequency-and-pitch/music-note-to-frequency-chart
     -- Fórmula: 25.000.000 / Frequência_da_Nota
     constant LUT_NOTAS : tabela_notas := (
-        0  => 28409,  -- Lá 4  (A4)  ->  880,00 Hz
-        1  => 26815,  -- Lá# 4 (A#4) -> ~932,33 Hz
-        2  => 25310,  -- Si 4  (B4)  -> ~987,77 Hz
-        3  => 23889,  -- Dó 5  (C5)  -> ~1046,50 Hz
-        4  => 22548,  -- Dó# 5 (C#5) -> ~1108,73 Hz
-        5  => 21283,  -- Ré 5  (D5)  -> ~1174,66 Hz
-        6  => 20088,  -- Ré# 5 (D#5) -> ~1244,51 Hz
-        7  => 18961,  -- Mi 5  (E5)  -> ~1318,51 Hz
-        8  => 17897,  -- Fá 5  (F5)  -> ~1396,91 Hz
-        9  => 16892,  -- Fá# 5 (F#5) -> ~1479,98 Hz
-        10 => 15944,  -- Sol 5 (G5)  -> ~1567,98 Hz
-        11 => 15049,  -- Sol# 5(G#5) -> ~1661,22 Hz
-        12 => 14205   -- Lá 5  (A5)  ->  1760,00 Hz
+        0  => 101239,  -- (B3)  ->  246,94 Hz
+        1  => 270270,  -- (F#2) ->  92,50 Hz	
+        2  => 67529 ,  -- (F#4) ->  369,99 Hz
+        3  => 255102,  -- (G2)  ->  98 Hz          NOTAS SWEDEN
+        4  => 90194 ,  -- (C#4) ->  277,18 Hz
+        5  => 227272,  -- (A2)  ->  110 Hz
+        6  => 75842 ,  -- (E4)  ->  329,63 Hz
+		  7  => 0	  ,  -- Nada
     );
 
     signal counter    : integer range 0 to 30000 := 0;
